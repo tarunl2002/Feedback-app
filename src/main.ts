e5aab3ddcfbacd8fd0a6e9c5125e3668d68b6be7
Tarun Lalwani <tarunlalwani2002@gmail.com>
2024-06-13 17:20:39 +0530

    Cors handled

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.enableCors({
+    origin: '*', 
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+
+  await app.listen(5000);
+  console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
